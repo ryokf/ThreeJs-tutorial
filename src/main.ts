39cdd1799bdf109aa5838a8da48bec1ef2166f7d
ryokf <ryokhrisnaf@gmail.com>
2024-12-07 12:12:48 +0700

    ✨feature: 2-camera

--- src/main.ts
+++ src/main.ts
@@ -1,97 +1,83 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import Stats from 'three/addons/libs/stats.module.js'
-import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import { GUI } from 'dat.gui'
 
+const scene = new THREE.Scene()
+scene.add(new THREE.GridHelper()) // uncomment to see a grid on the scene
 
-// Create a scene, which will hold all the elements like objects, cameras, and lights.
-// Set the background color of the scene to black.
-const sceneA = new THREE.Scene()
-sceneA.background = new THREE.Color(0xaaaaff)
-// set image as background
-const sceneB = new THREE.Scene()
-sceneB.background = new THREE.TextureLoader().load('https://sbcode.net/img/grid.png')
-// set 6 image as background
-const sceneC = new THREE.Scene()
-sceneC.background = new THREE.CubeTextureLoader().setPath('https://sbcode.net/img/').load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'])
+const camera = new THREE.PerspectiveCamera(
+  75, // field of view (in degrees)
+  window.innerWidth / window.innerHeight, // aspect ratio
+  0.1, // near clipping plane
+  1000, // far clipping plane
+)
+// camera.position.set(0, 2, 3) // uncomment to change the camera position
+// camera.lookAt(0, 0.5, 0) // uncomment to change the camera lookAt
 
-let activeScene = sceneA;
-const setScene = {
-  sceneA: () => {
-    activeScene = sceneA
-  },
-  sceneB: () => {
-    activeScene = sceneB
-  },
-  sceneC: () => {
-    activeScene = sceneC
-  },
-}
-
-// Set up a camera with a perspective view. It is positioned at a distance of 1.5 units along the z-axis.
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 1.5
-
-// Create a renderer to display the scene. The renderer's size is set to fill the window.
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer({
+  // antialias: true,
+  // alpha: true,
+  // logarithmicDepthBuffer: true,
+  // canvas: document.getElementById('canvas'),
+})
+// set the size of the canvas
 renderer.setSize(window.innerWidth, window.innerHeight)
+// add the canvas to the DOM
 document.body.appendChild(renderer.domElement)
 
-// Add an event listener to handle window resize events, updating the camera and renderer size.
+// update the camera and renderer when the window is resized
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
   renderer.setSize(window.innerWidth, window.innerHeight)
 })
 
-new OrbitControls(camera, renderer.domElement);
-
-// Create a box geometry and a normal material with wireframe for visualizing the cube.
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshNormalMaterial({ wireframe: true })
 
-// Create a mesh from the geometry and material, and add it to the scene.
 const cube = new THREE.Mesh(geometry, material)
+cube.position.y = 0.5 // move the cube up a bit to see the grid
+scene.add(cube)
 
-const stats = new Stats();
-document.body.appendChild(stats.dom);
+// add stats (FPS counter) to the page
+const stats = new Stats()
+document.body.appendChild(stats.dom)
 
-const gui = new GUI();
+// create a GUI to control the camera
+const gui = new GUI()
 
-const cubeRotationFolder = gui.addFolder('Cube Rotation');
-cubeRotationFolder.add(cube.rotation, 'x', -Math.PI, Math.PI);
-cubeRotationFolder.add(cube.rotation, 'y', -Math.PI, Math.PI);
-cubeRotationFolder.add(cube.rotation, 'z', -Math.PI, Math.PI);
+const cameraFolder = gui.addFolder('Camera')
+// add controls to the camera folder
+cameraFolder.add(camera.position, 'x', -10, 10)
+cameraFolder.add(camera.position, 'y', -10, 10)
+cameraFolder.add(camera.position, 'z', -10, 10)
+cameraFolder.add(camera, 'fov', 0, 180, 0.01).onChange(() => {
+  camera.updateProjectionMatrix()
+})
+cameraFolder.add(camera, 'aspect', 0.00001, 10).onChange(() => {
+  camera.updateProjectionMatrix()
+})
+cameraFolder.add(camera, 'near', 0.01, 10).onChange(() => {
+  camera.updateProjectionMatrix()
+})
+cameraFolder.add(camera, 'far', 0.01, 10).onChange(() => {
+  camera.updateProjectionMatrix()
+})
+cameraFolder.open()
 
-const cameraFolder = gui.addFolder('Camera');
-cameraFolder.add(camera.position, 'x', -5, 5);
-cameraFolder.add(camera.position, 'y', -5, 5);
-cameraFolder.add(camera.position, 'z', -5, 5);
-
-const sceneFolder = gui.addFolder('Scene');
-sceneFolder.add(activeScene, 'backgroundBlurriness', 0, 1).onChange((value) => {
-  activeScene.backgroundBlurriness = value
-});
-
-gui.add(setScene, 'sceneA').name('Scene A')
-gui.add(setScene, 'sceneB').name('Scene B')
-gui.add(setScene, 'sceneC').name('Scene C')
-
-// Define an animation function to rotate the cube and render the scene continuously.
+// animate the scene
 function animate() {
-  activeScene.add(cube)
+  // update the stats
+  stats.update()
+
+  camera.lookAt(cube.position)
+
+  // render the scene
+  renderer.render(scene, camera)
+
+  // request the next frame
   requestAnimationFrame(animate)
-
-  // Rotate the cube slightly on each frame around the x and y axes.
-  // cube.rotation.x += 0.01
-  // cube.rotation.y += 0.01
-
-  // Render the scene from the perspective of the camera.
-  renderer.render(activeScene, camera)
-
-  stats.update();
 }
 
-// Start the animation loop.
 animate()
