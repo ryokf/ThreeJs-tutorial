b618d886e4510cb39b3c3f77beb48d7050ebb762
ryokf <ryokhrisnaf@gmail.com>
2024-12-09 17:33:34 +0700

    ✨feature: 16-gltf loader

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,28 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 import { RGBELoader } from 'three/addons/loaders/RGBELoader.js'
 import Stats from 'three/addons/libs/stats.module.js'
+import { Lensflare, LensflareElement } from 'three/addons/objects/Lensflare.js'
 
 const scene = new THREE.Scene()
+
+const light = new THREE.SpotLight(undefined, Math.PI * 1000)
+light.position.set(5, 5, 5)
+light.angle = Math.PI / 16
+light.castShadow = true
+scene.add(light)
 
 new RGBELoader().load('img/venice_sunset_1k.hdr', (texture) => {
   texture.mapping = THREE.EquirectangularReflectionMapping
   scene.environment = texture
-  scene.background = texture
-  scene.backgroundBlurriness = 1.0
 })
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
-camera.position.set(2, 1, -2)
+camera.position.set(1.5, 0.75, 2)
 
 const renderer = new THREE.WebGLRenderer({ antialias: true })
 renderer.toneMapping = THREE.ACESFilmicToneMapping
+renderer.toneMappingExposure = 0.1
+renderer.shadowMap.enabled = true
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
@@ -29,34 +36,24 @@ window.addEventListener('resize', () => {
 })
 
 const controls = new OrbitControls(camera, renderer.domElement)
-controls.target.y = 0.75
 controls.enableDamping = true
 
-const loader = new GLTFLoader()
-loader.load('models/suv_body.glb', (gltf) => {
-  const suvBody = gltf.scene
-  gltf.scene.position.set(0, 0.5, 0)
-  loader.load('models/suv_wheel.glb', (gltf) => {
-    gltf.scene.position.set(-0.65, 0.2, -0.77)
-    suvBody.add(gltf.scene)
-  })
-  loader.load('models/suv_wheel.glb', (gltf) => {
-    gltf.scene.position.set(0.65, 0.2, -0.77)
-    gltf.scene.rotateY(Math.PI)
-    suvBody.add(gltf.scene)
-  })
-  loader.load('models/suv_wheel.glb', (gltf) => {
-    gltf.scene.position.set(-0.65, 0.2, 0.57)
-    suvBody.add(gltf.scene)
-  })
-  loader.load('models/suv_wheel.glb', (gltf) => {
-    gltf.scene.position.set(0.65, 0.2, 0.57)
-    gltf.scene.rotateY(Math.PI)
-    scene.add(gltf.scene)
-  })
+const textureLoader = new THREE.TextureLoader()
+const textureFlare0 = textureLoader.load('https://cdn.jsdelivr.net/gh/Sean-Bradley/First-Car-Shooter@main/dist/client/img/lensflare0.png')
+
+const lensflare = new Lensflare()
+lensflare.addElement(new LensflareElement(textureFlare0, 1000, 0))
+light.add(lensflare)
+
+new GLTFLoader().load('models/suzanne_scene.glb', (gltf) => {
+  const suzanne = gltf.scene.getObjectByName('Suzanne') as THREE.Mesh
+  suzanne.castShadow = true
+
+  const plane = gltf.scene.getObjectByName('Plane') as THREE.Mesh
+  plane.receiveShadow = true
+
   scene.add(gltf.scene)
 })
-
 
 const stats = new Stats()
 document.body.appendChild(stats.dom)
@@ -65,6 +62,7 @@ function animate() {
   requestAnimationFrame(animate)
 
   controls.update()
+
   renderer.render(scene, camera)
 
   stats.update()
