c850aac978dd40de9db96225020287f9683f88eb
ryokf <ryokhrisnaf@gmail.com>
2024-12-14 11:24:53 +0700

    ✨feature: 21-compress gltf

--- src/main.ts
+++ src/main.ts
@@ -1,145 +1,199 @@
-import "./style.css";
-import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-import Stats from "three/addons/libs/stats.module.js";
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js'
+import Stats from 'three/addons/libs/stats.module.js'
 
-const scene = new THREE.Scene();
+class CharacterController {
+  keyMap: { [key: string]: boolean } = {}
+  wait = false
+  animationActions: { [key: string]: THREE.AnimationAction }
+  activeAction = ''
+  speed = 0
 
-const gridHelper = new THREE.GridHelper(100, 100);
-scene.add(gridHelper);
+  constructor(animationActions: { [key: string]: THREE.AnimationAction }) {
+    this.animationActions = animationActions
+    document.addEventListener('keydown', this.onDocumentKey)
+    document.addEventListener('keyup', this.onDocumentKey)
+  }
 
-new RGBELoader().load("img/venice_sunset_1k.hdr", (texture) => {
-  texture.mapping = THREE.EquirectangularReflectionMapping;
-  scene.environment = texture;
-  scene.background = texture;
-  scene.backgroundBlurriness = 1;
-});
+  onDocumentKey = (e: KeyboardEvent) => {
+    this.keyMap[e.code] = e.type === 'keydown'
+  }
 
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  100
-);
-camera.position.set(0.1, 1, 1);
+  dispose() {
+    document.removeEventListener('keydown', this.onDocumentKey)
+    document.removeEventListener('keyup', this.onDocumentKey)
+  }
 
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+  setAction(action: string) {
+    if (this.activeAction != action) {
+      this.animationActions[this.activeAction].fadeOut(0.25)
+      this.animationActions[action].reset().fadeIn(0.25).play()
+      this.activeAction = action
 
-window.addEventListener("resize", () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-});
-
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.target.set(0, 0.75, 0);
-
-const stats = new Stats();
-document.body.appendChild(stats.dom);
-
-function lerp(from: number, to: number, speed: number) {
-  const amount = (1 - speed) * from + speed * to
-  return Math.abs(from - to) < 0.001 ? to : amount
-}
-
-let mixer: THREE.AnimationMixer;
-let animationActions: { [key: string]: THREE.AnimationAction } = {};
-let activeAction: THREE.AnimationAction;
-let speed = 0,
-  toSpeed = 0
-
-// new GLTFLoader().load("models/eve$@walk.glb", (gltf) => {
-//   mixer = new THREE.AnimationMixer(gltf.scene);
-//   console.log(mixer);
-
-//   mixer.clipAction(gltf.animations[0]).play();
-
-//   scene.add(gltf.scene);
-// });
-
-async function loadEve() {
-  const loader = new GLTFLoader();
-  const [eve, idle, run] = await Promise.all([
-    loader.loadAsync("models/eve$@walk.glb"),
-    loader.loadAsync("models/eve@idle.glb"),
-    loader.loadAsync("models/eve@run.glb"),
-  ]);
-
-  mixer = new THREE.AnimationMixer(eve.scene);
-
-  // mixer.clipAction(run.animations[1]).play()
-
-  animationActions["idle"] = mixer.clipAction(idle.animations[1]);
-  animationActions["walk"] = mixer.clipAction(eve.animations[0]);
-  animationActions["run"] = mixer.clipAction(run.animations[2]);
-
-  animationActions["idle"].play();
-  activeAction = animationActions["idle"];
-
-  scene.add(eve.scene);
-}
-await loadEve();
-
-const keyMap: { [key: string]: boolean } = {};
-
-const onDocumentKey = (e: KeyboardEvent) => {
-  keyMap[e.code] = e.type === "keydown";
-};
-document.addEventListener("keydown", onDocumentKey, false);
-document.addEventListener("keyup", onDocumentKey, false);
-
-const clock = new THREE.Clock();
-let delta = 0;
-
-function animate() {
-  requestAnimationFrame(animate);
-
-  delta = clock.getDelta();
-
-  controls.update();
-
-  mixer.update(delta);
-
-  if (keyMap["KeyW"]) {
-    if (keyMap["ShiftLeft"]) {
-      //run
-      if (activeAction != animationActions["run"]) {
-        activeAction.fadeOut(0.5);
-        animationActions["run"].reset().fadeIn(0.25).play();
-        activeAction = animationActions["run"];
-        toSpeed = 4
+      switch (action) {
+        case 'walk':
+          this.speed = 1
+          break
+        case 'run':
+        case 'jump':
+          this.speed = 4
+          break
+        case 'pose':
+        case 'idle':
+          this.speed = 0
+          break
       }
-    } else {
-      //walk
-      if (activeAction != animationActions["walk"]) {
-        activeAction.fadeOut(0.5);
-        animationActions["walk"].reset().fadeIn(0.25).play();
-        activeAction = animationActions["walk"];
-        toSpeed = 1
-      }
-    }
-  } else {
-    //idle
-    if (activeAction != animationActions["idle"]) {
-      activeAction.fadeOut(0.5);
-      animationActions["idle"].reset().fadeIn(0.25).play();
-      activeAction = animationActions["idle"];
-      toSpeed = 0
     }
   }
 
-  speed = lerp(speed, toSpeed, delta * 10)
-  gridHelper.position.z -= speed * delta
-  // gridHelper.position.z = gridHelper.position.z % 10
+  update() {
+    if (!this.wait) {
+      let actionAssigned = false
 
-  renderer.render(scene, camera);
+      if (this.keyMap['Space']) {
+        this.setAction('jump')
+        actionAssigned = true
+        this.wait = true // blocks further actions until jump is finished
+        setTimeout(() => (this.wait = false), 1000)
+      }
 
-  stats.update();
+      if (!actionAssigned && this.keyMap['KeyW'] && this.keyMap['ShiftLeft']) {
+        this.setAction('run')
+        actionAssigned = true
+      }
+
+      if (!actionAssigned && this.keyMap['KeyW']) {
+        this.setAction('walk')
+        actionAssigned = true
+      }
+
+      if (!actionAssigned && this.keyMap['KeyQ']) {
+        this.setAction('pose')
+        actionAssigned = true
+      }
+
+      !actionAssigned && this.setAction('idle')
+    }
+  }
 }
 
-animate();
+class Grid {
+  gridHelper = new THREE.GridHelper(100, 100)
+  speed = 0
+
+  constructor(scene: THREE.Scene) {
+    scene.add(this.gridHelper)
+  }
+
+  lerp(from: number, to: number, speed: number) {
+    const amount = (1 - speed) * from + speed * to
+    return Math.abs(from - to) < 0.001 ? to : amount
+  }
+
+  update(delta: number, toSpeed: number) {
+    this.speed = this.lerp(this.speed, toSpeed, delta * 10)
+    this.gridHelper.position.z -= this.speed * delta
+    this.gridHelper.position.z = this.gridHelper.position.z % 10
+  }
+}
+
+const scene = new THREE.Scene()
+
+new RGBELoader().load('img/venice_sunset_1k.hdr', (texture) => {
+  texture.mapping = THREE.EquirectangularReflectionMapping
+  scene.environment = texture
+  scene.background = texture
+  scene.backgroundBlurriness = 1
+})
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
+camera.position.set(0.1, 1, 1)
+
+const renderer = new THREE.WebGLRenderer({ antialias: true })
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+})
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+controls.target.set(0, 0.75, 0)
+
+const stats = new Stats()
+document.body.appendChild(stats.dom)
+
+let mixer: THREE.AnimationMixer
+let animationActions: { [key: string]: THREE.AnimationAction } = {}
+
+const characterController = new CharacterController(animationActions)
+const grid = new Grid(scene)
+
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/') // loading from a CDN
+//dracoLoader.setDecoderPath('jsm/libs/draco/') // loading from own webserver
+
+const glTFLoader = new GLTFLoader()
+glTFLoader.setDRACOLoader(dracoLoader)
+
+glTFLoader.load('models/eve$@walk_compressed.glb', (gltf) => {
+  mixer = new THREE.AnimationMixer(gltf.scene)
+
+  mixer.clipAction(gltf.animations[0]).play()
+
+  scene.add(gltf.scene)
+})
+
+// async function loadEve() {
+//   const [eve, idle, run, jump, pose] = await Promise.all([
+//     glTFLoader.loadAsync('models/eve$@walk_compressed.glb'),
+//     glTFLoader.loadAsync('models/eve@idle.glb'),
+//     glTFLoader.loadAsync('models/eve@run.glb'),
+//     glTFLoader.loadAsync('models/eve@jump.glb'),
+//     glTFLoader.loadAsync('models/eve@pose.glb')
+//   ])
+
+//   mixer = new THREE.AnimationMixer(eve.scene)
+
+//   animationActions['idle'] = mixer.clipAction(idle.animations[0])
+//   animationActions['walk'] = mixer.clipAction(eve.animations[0])
+//   animationActions['run'] = mixer.clipAction(run.animations[0])
+//   animationActions['jump'] = mixer.clipAction(jump.animations[0])
+//   animationActions['pose'] = mixer.clipAction(pose.animations[0])
+
+//   animationActions['idle'].play()
+//   characterController.activeAction = 'idle'
+
+//   scene.add(eve.scene)
+// }
+// await loadEve()
+
+const clock = new THREE.Clock()
+let delta = 0
+
+function animate() {
+  requestAnimationFrame(animate)
+
+  delta = clock.getDelta()
+
+  controls.update()
+
+  //characterController.update()
+
+  mixer && mixer.update(delta)
+
+  grid.update(delta, characterController.speed)
+
+  renderer.render(scene, camera)
+
+  stats.update()
+}
+
+animate()
